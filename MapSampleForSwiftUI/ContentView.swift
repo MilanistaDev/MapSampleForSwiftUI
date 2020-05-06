95b9df1630f308364374517ae699d29abe98f075
Takuya Aso <milanista224@gmail.com>
2020-05-06 19:15:46 +0900

    Initial Commit

---
+++ MapSampleForSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapSampleForSwiftUI
+//
+//  Created by Takuya Aso on 2020/05/06.
+//  Copyright © 2020 Takuya Aso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
